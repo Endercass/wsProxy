88be572bbce25a1aff9370ad75f13d10698f831d
endercass <46408156+Endercass@users.noreply.github.com>
2024-01-02 19:21:45 -0500

    Update mocha and fix test

--- test/init.js
+++ test/init.js
@@ -1,9 +1,23 @@
-var assert = require("assert");
+import Server from "../src/core/server.js";
+import cli_logger from "../src/cli_logger.js";
+import ModuleLoader from "../src/core/modules.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 describe("wsProxy", function () {
   it("Start server", function (done) {
+    const config = {
+      port: 5999,
+      ssl: false,
+      logger: cli_logger,
+      modules: new ModuleLoader(`${__dirname}/../src/modules`),
+    };
     try {
-      require("../index.js");
+      let server = new Server(config);
+      server.listen();
     } catch (e) {
       done(e);
     }
