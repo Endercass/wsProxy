124b3f4a80235119ad3b35bfc6154793f5183161
endercass <46408156+Endercass@users.noreply.github.com>
2024-01-02 20:07:57 -0500

    create index with exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Logger from "./core/logger.js";
+import Server from "./core/server.js";
+import ModuleLoader from "./core/modules.js";
+import SocketProxy from "./core/proxy.js";
+
+import cli_logger from "./cli_logger.js";
+
+export { Logger, Server, ModuleLoader, SocketProxy, cli_logger };
